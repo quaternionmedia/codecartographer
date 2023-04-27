8597aef15c925af3722a3fb9bf200a718da734cd
Cameron West <Cameron.west@hotmail.com>
2023-04-27 13:28:30 -0500

    Error Updates
    Updated errors, Black formatting applied.

--- codecarto/src/codecarto/errors.py
+++ codecarto/src/codecarto/errors.py
@@ -1,29 +1,40 @@
 class ThemeError(Exception):
     """Base class for exceptions in the Theme module."""
+
     pass
+
 
 class ThemeNotFoundError(ThemeError):
     """Raised when the specified theme is not found."""
+
     pass
+
 
 class BaseNotFoundError(ThemeError):
     """Raised when the specified base theme is not found."""
-    pass  
+
+    pass
+
 
 class ThemeCreationError(ThemeError):
     """Raised when a new theme could not be created."""
-    pass
 
+    pass
 
 
 class CliError(Exception):
     """Base class for CLI-related errors."""
-    pass 
+
+    pass
+
 
 class ThemesDirNotFoundError(CliError):
     """Raised when themes.json cannot be found."""
+
     pass
+
 
 class MissingParameterError(CliError):
     """Raised when a new command requires all parameters."""
+
     pass
